ed38862747fac4b010e874c218ec15dcf840d91a
Aldo Geovani Reyes Muñoz <ag.reyesm@live.com.mx>
2021-07-16 19:47:35 -0500

    End of section 7

---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  templateUrl: './test-errors.component.html',
+  styleUrls: ['./test-errors.component.css']
+})
+export class TestErrorsComponent implements OnInit {
+
+  baseUrl = 'https://localhost:5001/';
+  validationErrors : string[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  /** ERROR HANDLING COMPONENT */
+  get404Error() {
+    this.http.get(this.baseUrl + 'Buggy/not-found').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'Buggy/bad-request').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'Buggy/server-error').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get401Error() {
+    this.http.get(this.baseUrl + 'Buggy/auth').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'account/register',{}).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+      this.validationErrors = error;
+    });
+  }
+}
