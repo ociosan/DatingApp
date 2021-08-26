09fbce64abdcfc401c48898a3c0da136ab1c72fb
Aldo Geovani Reyes Muñoz <ag.reyesm@live.com.mx>
2021-08-25 19:23:34 -0500

    fix production mode enabled

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,6 +7,8 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+console.log('environment.production: ' + environment.production);
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
